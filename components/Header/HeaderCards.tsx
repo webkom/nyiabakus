8fc52578995f59694f10a6d71f442b747e7958cd
Jonatan Norbye <jonatan@norbye.com>
2023-05-25 16:14:22 +0200

    Separate card contents from JSX

--- components/Header/HeaderCards.tsx
+++ components/Header/HeaderCards.tsx
@@ -2,34 +2,42 @@ import Card from "@/components/Card";
 import styles from "./styles.module.css";
 import InfoSectionWrapper from "@/components/InfoSectionWrapper";
 
-const HeaderCards = () => {
+const HeaderCards: React.FC = () => {
   return (
     <InfoSectionWrapper className={styles.header}>
       <div className={styles.cardContainer}>
-        <Card
-          icon={"person"}
-          title={"Lag bruker på abakus.no"}
-          description={
-            "Meld deg på arrangementer og følg med på hva som skjer i Abakus!"
-          }
-        />
-        <Card
-          icon={"logo-slack"}
-          title={"Bli med på Slack"}
-          description={
-            "Hold kontakten med resten av Abakus! Heer har vi alt fra event-reminders, kjøp og salg, memes og forum for å finne sted å bo."
-          }
-        />
-        <Card
-          icon={"chatbox"}
-          title={"Meldeskjema"}
-          description={
-            "Har du blitt utsatt for en ubehagelig hendelse? Meld fra gjennom meldeskjemaet vårt! \nDu kan være helt anonym derson du ønsker det."
-          }
-        />
+        {cardData.map(({ title, icon, description }) => (
+          <Card
+            key={title}
+            icon={icon}
+            title={title}
+            description={description}
+          />
+        ))}
       </div>
     </InfoSectionWrapper>
   );
 };
 
 export default HeaderCards;
+
+const cardData = [
+  {
+    title: "Lag bruker på abakus.no",
+    icon: "person",
+    description:
+      "Meld deg på arrangementer og følg med på hva som skjer i Abakus!",
+  },
+  {
+    title: "Bli med på Slack",
+    icon: "logo-slack",
+    description:
+      "Hold kontakten med resten av Abakus! Heer har vi alt fra event-reminders, kjøp og salg, memes og forum for å finne sted å bo.",
+  },
+  {
+    title: "Meldeskjema",
+    icon: "chatbox",
+    description:
+      "Har du blitt utsatt for en ubehagelig hendelse? Meld fra gjennom meldeskjemaet vårt! \nDu kan være helt anonym derson du ønsker det.",
+  },
+];
