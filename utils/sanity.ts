efab9b194de76b2b328c192a79c6d1a926237b8f
Jonatan Norbye <jonatan@norbye.com>
2024-07-25 10:46:53 +0200

    Add sanity-token and use development database locally

---
+++ utils/sanity.ts
@@ -0,0 +1,14 @@
+import { createClient } from "next-sanity";
+
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const token = process.env.SANITY_TOKEN;
+
+export const sanityClient = createClient({
+  projectId,
+  dataset,
+  token,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: false,
+});
