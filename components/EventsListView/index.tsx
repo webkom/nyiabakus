815cc657a27f770f3703006b197087c509fa5b84
Isak Berg Endresen <isakbendresen@gmail.com>
2025-05-15 16:29:24 +0200

    Upgrade dependencies

--- components/EventsListView/index.tsx
+++ components/EventsListView/index.tsx
@@ -52,6 +52,7 @@ const EventsListView: React.FC<EventsListViewProps> = ({
   isLoadingEvents,
   expandDayDescriptionsByDefault,
 }) => {
+  console.log(dayDescriptions)
   const days = useMemo<Day[]>(() => {
     if (events.length === 0 && dayDescriptions.length === 0) return [];
     let firstDate: Date;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Ny i Abakus</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         {!router.asPath.includes("studio") && <Navbar />}
